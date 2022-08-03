1d01215408222cdf3dc3c643e0909c742fd033d4
“moHam <mohamgoham@gmail.com>
2022-08-02 21:26:16 -0400

    add user types

---
+++ Controllers/APIUserController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Unity_SDK.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class APIUserController : ControllerBase {
+    
+}
---
+++ User.cs
@@ -0,0 +1,17 @@
+﻿namespace Unity_SDK;
+
+public class User
+{
+    public string Id { get; set; }
+    public string Key { get; set; }
+    public string Address { get; set; }
+    public string WalletAddress { get; set; }
+    public string PrivayeKey { get; set; }
+    public int Nonce { get; set; }
+    //public string Wallet { get; set; }
+    public DateTime Wallet { get; set; }
+    public string Status { get; set; }
+    
+}
+
+
