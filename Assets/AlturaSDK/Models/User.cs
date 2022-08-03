0f4f29d05808b053a294d4e2dcd4796659c5c65c
brohamgoham <mohamgoham@gmail.com>
2022-08-03 16:24:40 -0400

    add user

---
+++ Assets/AlturaSDK/Models/User.cs
@@ -0,0 +1,21 @@
+
+
+using System;
+
+namespace AlturaSDK.Models
+{
+    [Serializable]
+    public class AlturaUser
+    {
+        public string address;        
+        
+        public string name;
+        public string bio;
+        
+        public string profilePic;
+        public string socialLink;
+        public string profilePicUrl;
+        public string balance;
+
+    }
+}
--- Controllers/APIUserController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace Unity_SDK.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class APIUserController : ControllerBase {
-    
-}
