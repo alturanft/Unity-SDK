ecead7582a60b214cf9f8e328499ee72d72d1dd7
brohamgoham <mohamgoham@gmail.com>
2022-08-04 13:40:52 -0400

    item collections and update User schema

---
+++ Assets/AlturaSDK/Models/ItemCollection.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace AlturaSDK.Models
+{
+    [Serializable]
+    public class itemCollection
+    {
+        //blockchain id
+        public string address;        
+        
+        public int chainId;
+        public int version;
+    
+        public string ownerAddress;
+        public string slug;
+
+        public int uri;
+        public bool isPublic;
+        public DateTime mintDate;
+        public string name;
+
+        public string description;
+        public string website;
+        public string genre;
+
+        // image
+        public string image;
+        public string imageUrl;
+        public string imageHash;
+
+        // holders and Volume
+        public string holders;
+        public string volume1d;
+        public string volume1W;
+        public string volume30d;
+        public string volumeAll;
+
+        public bool isVerified;
+        public bool featured;
+        public bool featuredMain;
+
+
+
+
+    }
+}
--- Assets/AlturaSDK/Models/User.cs
+++ Assets/AlturaSDK/Models/User.cs
@@ -5,15 +5,40 @@ namespace AlturaSDK.Models
     [Serializable]
     public class AlturaUser
     {
-        public string address;        
-        
+        public string address;   
+
+        /// <summary>
+        /// meatadata
+        /// </summary>
         public string name;
         public string bio;
-        
-        public string profilePic;
         public string socialLink;
+
+        /// <summary>
+        /// image
+        /// </summary>
+        public string imageHash;
+        public string profilePic;
         public string profilePicUrl;
-        public string balance;
+
+        /// <summary>
+        /// login & security
+        /// </summary>
+        public int nonce;
+        public DateTime lastLogin;
+
+        /// <summary>
+        /// Altura Guard code
+        /// </summary>
+        public string authCode;
+
+        /// <summary>
+        /// Sys flags
+        /// </summary>
+        public bool admin;
+        public bool agreeToTerms;
+        public bool blacklisted;
+
 
     }
 }
