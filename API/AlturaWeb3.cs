1806cbb16f27b31b5c89f753e854d343534f3b8a
Mohammed M <mohamgoham@gmail.com>
2022-10-26 16:30:07 -0400

    add meta file

--- API/AlturaWeb3.cs
+++ API/AlturaWeb3.cs
@@ -2,25 +2,12 @@ using System.Collections;
 using Newtonsoft.Json;
 using UnityEngine;
 
-// create AlturaWeb3().GetItem(), AlturaWeb3().MintAdditionalNFT(), AlturaWeb3().TransferItem()
 namespace AlturaNFT
 {
     using Internal;
     public class AlturaWeb3
     {
-        public GetUsers Users_Details()
-        {
-            return new GetUsers();
-        }
-        public GetItems GetItems()
-        {
-            return new GetItems();
-        }
 
-        public GetItem NFT_Details()
-        {
-            return new GetItems();
-        }
         public MintAdditionalNFT MintAdditionalNFT()
         {
             return new MintAdditionalNFT();
