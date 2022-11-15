b3999c15610111769f9fa61c363d86af4078a5d4
Mohammed M <mohamgoham@gmail.com>
2022-11-15 11:44:05 -0500

    transferDemo

--- Altura-Sample/AlturaDemo/GetUserDetails.cs
+++ Altura-Sample/AlturaDemo/GetUserDetails.cs
@@ -10,28 +10,54 @@ namespace AlturaNFT.Samples.AlturaDemo{
     // get user details
     public class GetUserDetails : MonoBehaviour
     {
+
+        #region TransferItem
+
         [SerializeField]
-        GetUsers usersDetails;
+        private string address;
 
-        [SerializeField] 
-        private Dropdown chainDropdown;
+        [SerializeField]
+        private string tokenId;
 
-        [SerializeField] private Text accountAddressText;
-        [SerializeField] private Text contractFilter;
+        [SerializeField]
+        private string to;
+
+        [SerializeField]
+        private string amount;
         [SerializeField] private Text outputWindow;
+
         
-        public void UsersDetails_Run()
+        public void TransferItem_Run()
         {
-            usersDetails
+            TransferItem
+                .Initialize(destroyAtEnd: true)
+                .SetParameters(
+                  collection_addr: address,
+                  token_id: tokenId,
+                  amount: amount,
+                  to_addr: to
+                )
+                .OnError(error => Debug.Log(error))
+                .OnComplete(result => Debug.Log(result))
                 .Run();
+
+        }
+
+        void TransferItem_Output(Transfer_model transferModel)
+        {
+            Debug.Log("TransferItem_Output: " + transferModel.txHash);
+            outputWindow.text = "";
+
+            outputWindow.text += "TransferItem_Output: " + transferModel.txHash + "\n";
         }
 
 
-
+    #endregion
   
 
         private void Start()
         {
+            TransferItem_Run();
         }
     }
 
