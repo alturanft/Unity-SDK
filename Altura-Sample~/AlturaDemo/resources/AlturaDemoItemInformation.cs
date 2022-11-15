d2ade3320d1a7abc6d7460f22a090261f26de0a9
Abolfazl Arab <mranoncoder@gmail.com>
2022-11-15 22:39:55 +0100

    Added new Demo Scene

---
+++ Altura-Sample~/AlturaDemo/resources/AlturaDemoItemInformation.cs
@@ -0,0 +1,56 @@
+namespace AlturaNFT.Samples.AlturaDemo
+{
+    using UnityEngine;
+    using AlturaNFT;
+    using UnityEngine.Events;
+    using UnityEngine.UI;
+
+    // get user details
+    public class AlturaDemoItemInformation : MonoBehaviour
+    {
+
+        #region ItemInformation
+
+        public Button _getdataButton;
+        [SerializeField]
+        public InputField _address;
+
+        [SerializeField]
+        public Text NameInfo;
+
+        [SerializeField]
+        public Text BioInfo;
+        [SerializeField]
+        public GameObject UserInformationObject;
+
+        #endregion
+
+        private void Start()
+        {
+            Button btn = _getdataButton.GetComponent<Button>();
+            btn.onClick.AddListener(GetDataFunction);
+        }
+
+        public void GetDataFunction()
+        {
+
+            GetUser
+              .Initialize(destroyAtEnd: true)
+              .SetParameters(
+                _address.text.ToString()
+              ).OnError(error => Debug.Log(error))
+              .OnComplete(result => setData(result)
+              )
+              .Run();
+
+        }
+        private void setData(Internal.User_model _result)
+        {
+            NameInfo.text = _result.user.name;
+            BioInfo.text = _result.user.bio;
+            UserInformationObject.SetActive(true);
+        }
+
+    }
+
+}
